fb8bd8de40579ffb1b2fcc090addb1728131425e
Jason Diaz Jimenez <vulpothedev@gmail.com>
2021-07-18 17:50:36 -0500

    Quick Fix on userinfo

--- src/commands/Information/userinfo.ts
+++ src/commands/Information/userinfo.ts
@@ -1,5 +1,5 @@
 import HozolClient from '../../lib/HozolClient';
-import { GuildMember, Message, MessageEmbed, Role, User } from 'discord.js';
+import { GuildMember, Message, MessageEmbed, Role } from 'discord.js';
 
 import { Command } from 'nukejs';
 import { usernameResolver } from './../../helper/resolvers/usernameResolver';
@@ -18,8 +18,7 @@ module.exports = class extends Command {
             botPerms: ['SEND_MESSAGES', 'EMBED_LINKS'],
             description: 'See information on a Guild Member.',
             enabled: true,
-            extendedHelp:
-                "See infomration about a Guild Member's Join/Leave Date, Avatar, Roles, Connected Networks.",
+            extendedHelp: "See infomration about a Guild Member's Join/Leave Date, Avatar, Roles, Connected Networks.",
             usage: '[Username|Mention|ID]',
         });
     }
@@ -34,18 +33,11 @@ module.exports = class extends Command {
         let user;
         const allowedPerms: any[] = [];
         const disallowedPerms: any[] = [];
-
-        if (args) {
-            await usernameResolver(message, args.slice(0).join(' ')).then(
-                (resolved: User) => {
-                    user = resolved;
-                }
-            );
-        }
+        if (!args) user = message.author;
+        else user = usernameResolver(message, args.slice(0).join(' '));
         const member =
             // @ts-ignore
-            <GuildMember>message.guild?.members.cache.get(user?.id) ||
-            message.member;
+            <GuildMember>message.guild?.members.cache.get(user?.id) || message.member;
         let rolemap: any[] | any = member?.roles.cache
             .sort((a: Role, b: Role) => b.position - a.position)
             .map((r) => r)
@@ -64,22 +56,15 @@ module.exports = class extends Command {
                 )
                 .addField(
                     'Joined At',
-                    `${client.moment(joinDate).format('LLLL')} (${client
-                        .moment(joinDate)
-                        .fromNow()})`
+                    `${client.moment(joinDate).format('LLLL')} (${client.moment(joinDate).fromNow()})`
                 )
                 .addField(
                     'Registered At:',
-                    `${client.moment(createdDate).format('LLLL')} (${client
-                        .moment(createdDate)
-                        .fromNow()})`
+                    `${client.moment(createdDate).format('LLLL')} (${client.moment(createdDate).fromNow()})`
                 )
                 .addField('User ID', member.id)
                 .addField('Roles', rolemap)
-                .setThumbnail(
-                    member.user.avatarURL({ dynamic: true }) ||
-                        member.user.defaultAvatarURL
-                )
+                .setThumbnail(member.user.avatarURL({ dynamic: true }) || member.user.defaultAvatarURL)
                 .setColor(colors(member.id))
                 .setTimestamp()
                 .setFooter(`Requester ID: ${message.author.id}`);
@@ -118,27 +103,17 @@ module.exports = class extends Command {
             ];
             const perms = member.permissions.toArray();
             perms.forEach((perm) => {
-                allowedPerms.push(
-                    allPermissions.splice(allPermissions.indexOf(perm), 1)
-                );
+                allowedPerms.push(allPermissions.splice(allPermissions.indexOf(perm), 1));
             });
             disallowedPerms.push(...allPermissions);
 
             embed.addField(
                 'Allowed Permissions',
-                `${
-                    allowedPerms.length > 0
-                        ? `✅ \`${allowedPerms.join('`, `')}\``
-                        : 'No Allowed Permission'
-                }`
+                `${allowedPerms.length > 0 ? `✅ \`${allowedPerms.join('`, `')}\`` : 'No Allowed Permission'}`
             );
             embed.addField(
                 'Disallowed Permissions',
-                `${
-                    disallowedPerms.length > 0
-                        ? `❌ \`${disallowedPerms.join('`, `')}\``
-                        : 'No Disallowed Permission'
-                }`
+                `${disallowedPerms.length > 0 ? `❌ \`${disallowedPerms.join('`, `')}\`` : 'No Disallowed Permission'}`
             );
             await message.channel.send(embed);
         }
